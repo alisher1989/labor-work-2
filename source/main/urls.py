ba04783588a662931ae365e6786d74ae2b3d2e4d
Alisher <shera.rashiduulu@gmail.com>
2019-09-30 12:29:19 +0600

    created CRUD

---
+++ source/main/urls.py
@@ -0,0 +1,35 @@
+"""main URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from webapp.views import IndexView, ArticleView, ArticleCreateView, ArticleUpdateView, \
+    ArticleDeleteView, CommentCreateView, CommentsView, CommentUpdateView, CommentDeleteView, NewCommentCreateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
+    path('article/<int:pk>/', ArticleView.as_view(), name='article_view'),
+    path('article/add/', ArticleCreateView.as_view(), name='article_add'),
+    path('article/<int:pk>/update/', ArticleUpdateView.as_view(), name='article_update'),
+    path('article/<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+    path('comment/add/', CommentCreateView.as_view(), name='comment_add'),
+    path('comment/add/<int:pk>/', NewCommentCreateView.as_view(), name='comment_article'),
+    path('comment/', CommentsView.as_view(), name='comment_view'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
+
+]
---
+++ source/webapp/views.py
@@ -0,0 +1,166 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from webapp.forms import ArticleForm, CommentForm, CommentArticleForm
+from webapp.models import Article, Comment
+from django.views import View
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['articles'] = Article.objects.all()
+        return context
+
+
+class ArticleView(TemplateView):
+    template_name = 'article.html'
+
+    def get_context_data(self, **kwargs):
+        pk = kwargs.get('pk')
+        form = CommentArticleForm()
+        context = super().get_context_data(**kwargs)
+        context['article'] = get_object_or_404(Article, pk=pk)
+        context['form'] = form
+        return context
+
+
+class ArticleCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'create.html', context={'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(data=request.POST)
+        if form.is_valid():
+            article = Article.objects.create(
+                title=form.cleaned_data['title'],
+                author=form.cleaned_data['author'],
+                text=form.cleaned_data['text'],
+                category=form.cleaned_data['category']
+            )
+            return redirect('article_view', pk=article.pk)
+        else:
+            return render(request, 'create.html', context={'form': form})
+
+
+class ArticleUpdateView(View):
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, pk=kwargs.get('pk'))
+        form = ArticleForm(data={
+            'title': article.title,
+            'author': article.author,
+            'text': article.text,
+            'category': article.category_id
+        })
+        return render(request, 'update.html', context={'form': form, 'article': article})
+
+    def post(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, pk=kwargs.get('pk'))
+        form = ArticleForm(data=request.POST)
+        if form.is_valid():
+            article.title = form.cleaned_data['title']
+            article.author = form.cleaned_data['author']
+            article.text = form.cleaned_data['text']
+            article.category = form.cleaned_data['category']
+            article.save()
+            return redirect('article_view', pk=article.pk)
+        else:
+            return render(request, 'update.html', context={'form': form, 'article': article})
+
+
+
+class ArticleDeleteView(View):
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, pk=kwargs.get('pk'))
+        return render(request, 'delete.html', context={'article': article})
+
+    def post(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, pk=kwargs.get('pk'))
+        article.delete()
+        return redirect('index')
+
+
+
+class CommentsView(TemplateView):
+    template_name = 'comments/comments.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment'] = Comment.objects.all().order_by('-created_at')
+        print(Comment.objects.all().order_by('-created_at'))
+        return context
+
+
+
+class CommentCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = CommentForm()
+        return render(request, 'comments/create.html', context={'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = CommentForm(data=request.POST)
+        if form.is_valid():
+            comment = Comment.objects.create(
+                author=form.cleaned_data['author'],
+                text=form.cleaned_data['text'],
+                article=form.cleaned_data['article']
+            )
+            return redirect('article_view', pk=comment.article.pk)
+        else:
+            return render(request, 'comments/create.html', context={'form': form})
+
+
+class NewCommentCreateView(View):
+
+    def post(self, request, *args, **kwargs):
+        form = CommentArticleForm(data=request.POST)
+        if form.is_valid():
+            comment = Comment.objects.create(
+                author=form.cleaned_data['author'],
+                text=form.cleaned_data['text'],
+                article_id=kwargs.get('pk')
+            )
+            return redirect('article_view', pk=comment.article.pk)
+        else:
+            return render(request, 'comments/create.html', context={'form': form})
+
+
+class CommentUpdateView(View):
+    def get(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs.get('pk'))
+        form = CommentForm(data={
+            'author': comment.author,
+            'text': comment.text,
+            'article': comment.article_id
+        })
+        return render(request, 'comments/update.html', context={'form': form, 'comment': comment})
+
+    def post(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs.get('pk'))
+        form = CommentForm(data=request.POST)
+        if form.is_valid():
+            comment.author = form.cleaned_data['author']
+            comment.text = form.cleaned_data['text']
+            comment.article = form.cleaned_data['article']
+            comment.save()
+            return redirect('article_view', pk=comment.pk)
+        else:
+            return render(request, 'comments/update.html', context={'form': form, 'comment': comment})
+
+
+
+class CommentDeleteView(View):
+    def get(self, request, pk, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=pk)
+        return render(request, 'comments/delete.html', context={'comment': comment})
+
+
+    def post(self, request, pk, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=pk)
+        try:
+            comment.delete()
+            return redirect('comment_view')
+        except:
+            return render(request, 'comments/comments.html')
