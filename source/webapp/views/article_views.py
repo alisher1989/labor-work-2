fc3fbebf4f7183c5b3116aff5ac506d340fb84e3
Alisher <shera.rashiduulu@gmail.com>
2019-10-05 12:42:01 +0600

    Changed article_views.py

--- source/webapp/views/article_views.py
+++ source/webapp/views/article_views.py
@@ -1,74 +1,51 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from webapp.forms import ArticleForm, CommentArticleForm
-from webapp.models import Article, Comment
+from django.urls import reverse
 from django.views import View
-from django.views.generic import TemplateView, ListView, DetailView
-from django.core.paginator import Paginator
+from django.views.generic import ListView, DetailView, CreateView
 
+from webapp.forms import ArticleForm, ArticleCommentForm
+from webapp.models import Article
+from django.core.paginator import Paginator
 
 
 class IndexView(ListView):
     template_name = 'article/index.html'
-    model = Article
     context_object_name = 'articles'
+    model = Article
     ordering = ['-created_at']
-    paginate_by = 3
+    paginate_by = 5
     paginate_orphans = 1
-
-
-
-
-    # def get_queryset(self):
-    #     return super().get_queryset().order_by('-created_at')
 
 
 class ArticleView(DetailView):
     template_name = 'article/article.html'
-    pk_url_kwarg = 'article_pk'
     model = Article
 
-    class ArticleView(TemplateView):
-        template_name = 'article/article.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        article = self.object
+        context['form'] = ArticleCommentForm()
+        comments = article.comments.order_by('-created_at')
+        self.paginate_comments_to_context(comments, context)
+        return context
 
-        def get_context_data(self, **kwargs):
-            context = super().get_context_data(**kwargs)
-            pk = kwargs.get('pk')
-            article = get_object_or_404(Article, pk=pk)
-            context['article'] = article
-            context['form'] = CommentArticleForm()
-            comments = article.comments.order_by('-created_at')
-            self.paginate_comments_to_context(comments, context)
-            return context
-
-        def paginate_comments_to_context(self, comments, context):
-            paginator = Paginator(comments, 3, 0)
-            page_number = self.request.GET.get('page', 1)
-            page = paginator.get_page(page_number)
-            context['paginator'] = paginator
-            context['page_obj'] = page
-            context['comments'] = page.object_list
-            context['is_paginated'] = page.has_other_pages()
-            return context
+    def paginate_comments_to_context(self, comments, context):
+        paginator = Paginator(comments, 3, 0)
+        page_number = self.request.GET.get('page', 1)
+        page = paginator.get_page(page_number)
+        context['paginator'] = paginator
+        context['page_obj'] = page
+        context['comments'] = page.object_list
+        context['is_paginated'] = page.has_other_pages()
 
 
+class ArticleCreateView(CreateView):
+    form_class = ArticleForm
+    model = Article
+    template_name = 'article/create.html'
 
-class ArticleCreateView(View):
-    def get(self, request, *args, **kwargs):
-        form = ArticleForm()
-        return render(request, 'article/create.html', context={'form': form})
-
-    def post(self, request, *args, **kwargs):
-        form = ArticleForm(data=request.POST)
-        if form.is_valid():
-            article = Article.objects.create(
-                title=form.cleaned_data['title'],
-                author=form.cleaned_data['author'],
-                text=form.cleaned_data['text'],
-                category=form.cleaned_data['category']
-            )
-            return redirect('article_view', pk=article.pk)
-        else:
-            return render(request, 'article/create.html', context={'form': form})
+    def get_success_url(self):
+        return reverse('article_view', kwargs={'pk': self.object.pk})
 
 
 class ArticleUpdateView(View):
@@ -94,7 +71,6 @@ class ArticleUpdateView(View):
             return redirect('article_view', pk=article.pk)
         else:
             return render(request, 'article/update.html', context={'form': form, 'article': article})
-
 
 
 class ArticleDeleteView(View):
